8a7a5172f042bb8738260551b199c516b15ebaa7
saroz rimal <sarojrimal>
2019-12-30 04:54:19 +0545

    final product

---
+++ car/db/cars.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2018 at 08:51 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cars`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `uname`, `pass`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `car_id` int(11) NOT NULL,
+  `car_name` varchar(255) NOT NULL,
+  `car_type` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `hire_cost` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`car_id`, `car_name`, `car_type`, `image`, `hire_cost`, `capacity`, `status`) VALUES
+(1, 'Mercedes Benz', 'Mercedes Benz', 'car1.jpg', 20000, 5, 'Available'),
+(2, 'Range Rover', 'LandRover', 'car2.jpg', 30000, 6, 'Available'),
+(5, 'LandCruiser V8', 'LandCruiser ', 'images (2).jpg', 20000, 5, 'Available'),
+(6, 'Security Vehicles', 'Hammar Cars', 'sonkort2.png', 30000, 8, 'Available'),
+(7, 'Wedding Limousine', 'Wedding Limousine', 'images (3).jpg', 50000, 10, 'Available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `client_id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `id_no` int(11) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `mpesa` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`client_id`, `fname`, `email`, `id_no`, `phone`, `location`, `gender`, `car_id`, `status`, `mpesa`) VALUES
+(5, 'shrawan dhakal', 'shreyamjn@gmail.com', 1, 2147483647, 'ktm', 'Male', 0, 'Available', '22234567890'),
+(6, 'saman nath', 'samannath@gmail.com', 54321, 2147483647, 'pokhara', 'Male', 0, 'Available', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hire`
+--
+
+CREATE TABLE `hire` (
+  `hire_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `car_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `msg_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`msg_id`, `client_id`, `message`, `status`, `time`) VALUES
+(6, 0, 'the services was quality and affordable', 'Unread', '2018-11-26 08:52:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`car_id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `hire`
+--
+ALTER TABLE `hire`
+  ADD PRIMARY KEY (`hire_id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `hire`
+--
+ALTER TABLE `hire`
+  MODIFY `hire_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ car/includes/footer.php
@@ -0,0 +1,28 @@
+	<li class="about">
+
+<p>carcompany is an organized company that rents cars and other vehicles to clients at lower costs. We we are here to serve each every Citizen</p>
+
+					<ul>
+						
+<li><a href="#" class="facebook" target="_blank"></a></li>
+						
+<li><a href="#" class="twitter" target="_blank"></a></li>
+						
+<li><a href="#" class="google" target="_blank"></a></li>
+						
+<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		
+<div class="copyrights wrapper">
+			
+Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "http://localhost/car/">carcompany</a>
+		
+</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
